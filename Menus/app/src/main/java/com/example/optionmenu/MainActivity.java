d4c596cf9adf50c34576390b8c82c5f2fc6a9470
kiskue <81956078+kiskue@users.noreply.github.com>
2021-12-07 18:54:53 +0800

    commit

--- Menus/app/src/main/java/com/example/optionmenu/MainActivity.java
+++ Menus/app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.optionmenu;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    Button btnpopup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnpopup = (Button)findViewById(R.id.popup);
+        button = findViewById(R.id.button);
+        registerForContextMenu(button);
+        btnpopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, button);
+                popupMenu.getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu());
+
+                popupMenu.show();
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this, ""+item.getTitle(), Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            getMenuInflater().inflate(R.menu.contextmenu,menu);
+            super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, ""+item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.opyion_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Option1:
+                Toast.makeText(this, "Option1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Option2:
+                Toast.makeText(this, "Option2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
