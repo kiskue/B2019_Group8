3e0b897451e96ca8f6c72b5843fbb473a4b7ea20
kiskue <81956078+kiskue@users.noreply.github.com>
2021-12-21 18:02:29 +0800

    commit

---
+++ FinalProject/app/src/main/java/com/example/subdivisionemergencyapp/MainActivity2.java
@@ -0,0 +1,49 @@
+package com.example.subdivisionemergencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+
+
+            }
+
+
+        });
+
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+
+
+            }
+
+
+        });
+    }
+}
